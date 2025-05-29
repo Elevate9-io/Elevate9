730e11d066c7b465aacaad0bc036fa1acf9cd172
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-05-29 14:13:00 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,36 +28,44 @@ export default function RootLayout({ children }) {
                 <AppRouterCacheProvider options={{ enabledCssLayer: true }}>
                     <Sidebar />
                     <div className="flex flex-col min-h-screen">
-                        <AppBar position="static" elevation={1} sx={{ backgroundColor: 'black' }}>
+                        <AppBar position="sticky" elevation={1} sx={{ backgroundColor: 'black' }}>
                             <Container maxWidth="xl">
                                 <Toolbar disableGutters>
-                                    <Typography
-                                        variant="h6"
-                                        noWrap
-                                        component="div"
-                                        sx={{ flexGrow: 1, fontWeight: 700 }}
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            width: '100%',
+                                            gap: 4, // spacing between items
+                                        }}
                                     >
-                                        Elevate9
-                                    </Typography>
-                                    <Box sx={{ display: 'flex', gap: 2 }}>
-                                        <Link href="/" passHref legacyBehavior>
-                                            <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Home</Typography>
-                                        </Link>
-                                        <Link href="#section2" passHref legacyBehavior>
-                                            <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>About Us</Typography>
-                                        </Link>
-                                        <Link href="#section3" passHref legacyBehavior>
-                                            <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Services</Typography>
-                                        </Link>
-                                        <Link href="#section4" passHref legacyBehavior>
-                                            <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Portfolio</Typography>
-                                        </Link>
-                                        <Link href="#section5" passHref legacyBehavior>
-                                            <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Contact Us</Typography>
-                                        </Link>
-                                        <Link href="#section6" passHref legacyBehavior>
-                                            <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>FAQs</Typography>
-                                        </Link>
+                                        <Box
+                                            component="img"
+                                            src="/images/Elevate9-Logo-2.png"
+                                            alt="Logo"
+                                            sx={{ height: 40 }}
+                                        />
+                                        <Box sx={{ display: 'flex', gap: 2 }}>
+                                            <Link href="/" passHref legacyBehavior>
+                                                <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Home</Typography>
+                                            </Link>
+                                            <Link href="#section2" passHref legacyBehavior>
+                                                <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>About Us</Typography>
+                                            </Link>
+                                            <Link href="#section3" passHref legacyBehavior>
+                                                <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Services</Typography>
+                                            </Link>
+                                            <Link href="#section4" passHref legacyBehavior>
+                                                <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Portfolio</Typography>
+                                            </Link>
+                                            <Link href="#section5" passHref legacyBehavior>
+                                                <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>Contact Us</Typography>
+                                            </Link>
+                                            <Link href="#section6" passHref legacyBehavior>
+                                                <Typography component="a" sx={{ color: 'inherit', textDecoration: 'none', fontWeight: 500, px: 2, py: 1, '&:hover': { color: 'primary.main' } }}>FAQs</Typography>
+                                            </Link>
+                                        </Box>
                                     </Box>
                                 </Toolbar>
                             </Container>
