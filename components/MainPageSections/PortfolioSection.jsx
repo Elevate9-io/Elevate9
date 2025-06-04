63a691605c236cd9bd8fa453d0a6e8b8607b1407
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-06-04 12:43:15 -0500

    Update PortfolioSection.jsx

--- components/MainPageSections/PortfolioSection.jsx
+++ components/MainPageSections/PortfolioSection.jsx
@@ -4,28 +4,28 @@ export default function PortfolioSection() {
     return (
         <section id="section4" className="content-section">
             <div className="section-wrapper">
-            <div className="section4-banner">
-                <div className="section4-content">
-                <div className="section4-text">
-                    <h1 className="section4-h1">Portfolio - Transformation That Speak</h1>
-                    <p className="section4-p">Building a brand goes beyond aesthetics—it&apos;s about clarity, consistency, and purpose.</p>
-                    <div className="section4-flex">
-                    <div className="section4-flex-item">
-                        <img src="/images/PortfolioImage.png" alt="" className="section4-img" />
-                    </div>
-                    <div className="section4-flex-item">
-                        <img src="/images/PortfolioImage2.png" alt="" className="section4-img" />
-                    </div>
-                    <div className="section4-flex-item">
-                        <img src="/images/PortfolioImage3.png" alt="" className="section4-img" />
-                    </div>
-                    <div className="section4-flex-item">
-                        <img src="/images/PortfolioImage4.png" alt="" className="section4-img" />
-                    </div>
+                <div className="section4-banner">
+                    <div className="section4-content">
+                        <div className="section4-text">
+                            <h1 className="section4-h1">Portfolio - Transformation That Speak</h1>
+                            <p className="section4-p">Building a brand goes beyond aesthetics—it&apos;s about clarity, consistency, and purpose.</p>
+                            <div className="section4-flex">
+                                <div className="section4-flex-item">
+                                    <img src="/images/PortfolioImage.png" alt="" className="section4-img" />
+                                </div>
+                                <div className="section4-flex-item">
+                                    <img src="/images/PortfolioImage2.png" alt="" className="section4-img" />
+                                </div>
+                                <div className="section4-flex-item">
+                                    <img src="/images/PortfolioImage3.png" alt="" className="section4-img" />
+                                </div>
+                                <div className="section4-flex-item">
+                                    <img src="/images/PortfolioImage4.png" alt="" className="section4-img" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            </div>
             </div>
         </section>
     );
