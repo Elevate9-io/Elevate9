58c02b614263c7ca91b5524d3da83f9030b87ee5
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-06-04 12:43:17 -0500

    Update ServicesSection.jsx

--- components/MainPageSections/ServicesSection.jsx
+++ components/MainPageSections/ServicesSection.jsx
@@ -4,84 +4,86 @@ export default function ServicesSection() {
     return (
             <section id="section3" className="content-section">
                 <div className="section-wrapper">
-                <div className="section3-banner">
-                    <div className="section3-content">
-                    <h1 className="section3-h1">Solution and Services</h1>
-                    
-                    <div className="section3-flex">
-                        <div className="section3-flex-top">
-                        <div className="section3-flex-item">
-                            <div className="flex-container">
-                            <div className="img-cont">
-                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon.png" alt="" className="section3-icon" />
-                            </div>
-                            <div className="text-cont">
-                                <div className="text">
-                                <h2 className="section3-flex-h2">Branding</h2>
-                                <p className="section3-flex-p">It&apos;s the story you tell, the emotion you evoke, and the presence you build. We co-create a true identity that aligns with your goals and makes your brand stand out.</p>
+                    <div className="section3-banner">
+                        <div className="section3-content">
+                            <h1 className="section3-h1">Solution and Services</h1>
+                            
+                            <div className="section3-flex">
+                                <div className="section3-flex-top">
+                                    <div className="section3-flex-item">
+                                        <div className="flex-container">
+                                            <div className="img-cont">
+                                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon.png" alt="" className="section3-icon" />
+                                            </div>
+                                            <div className="text-cont">
+                                                <div className="text">
+                                                    <h2 className="section3-flex-h2">Branding</h2>
+                                                    <p className="section3-flex-p">It&apos;s the story you tell, the emotion you evoke, and the presence you build. We co-create a true identity that aligns with your goals and makes your brand stand out.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="section3-flex-item">
+                                        <div className="flex-container">
+                                            <div className="img-cont">
+                                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon4.png" alt="" className="section3-icon" />
+                                            </div>
+                                            <div className="text-cont">
+                                                <div className="text">
+                                                    <h2 className="section3-flex-h2">Social media content</h2>
+                                                    <p className="section3-flex-p">
+                                                        Social media content isn&apos;t just about aesthetics.
+                                                        It&apos;s about creating a lasting connection with your audience.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="section3-flex-bottom">
+                                    <div className="section3-flex-item">
+                                        <div className="flex-container">
+                                            <div className="img-cont">
+                                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon2.png" alt="" className="section3-icon" />
+                                            </div>
+                                            <div className="text-cont">
+                                                <div className="text">
+                                                    <h2 className="section3-flex-h2">Web Development</h2>
+                                                    <p className="section3-flex-p">Custom web development gives you the power to create a website that&apos;s perfectly aligned with your business goals. It offers flexibility, scalability, and full control over user experience and performance.</p>
+                                                </div> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="section3-flex-item">
+                                        <div className="flex-container">
+                                            <div className="img-cont">
+                                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon3.png" alt="" className="section3-icon" />
+                                            </div>
+                                            <div className="text-cont">
+                                                <div className="text">
+                                                    <h2 className="section3-flex-h2">Email Marketing</h2>
+                                                    <p className="section3-flex-p">Email marketing has long been one of the most effective tools in digital marketing.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="section3-flex-item">
+                                        <div className="flex-container">
+                                            <div className="img-cont">
+                                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon5.png" alt="" className="section3-icon" />
+                                            </div>
+                                            <div className="text-cont">
+                                                <div className="text">
+                                                    <h2 className="section3-flex-h2">Advertising and analytics</h2>
+                                                    <p className="section3-flex-p">At Elevate9, we create tailor-made advertising campaigns to help brands stand out and deliver tangible results.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            </div>
-                        </div>
-                        <div className="section3-flex-item">
-                            <div className="flex-container">
-                            <div className="img-cont">
-                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon4.png" alt="" className="section3-icon" />
-                            </div>
-                            <div className="text-cont">
-                                <div className="text">
-                                <h2 className="section3-flex-h2">Social media content</h2>
-                                <p className="section3-flex-p">Social media content isn&apos;t just about aesthetics.
-                                It&apos;s about creating a lasting connection with your audience.</p>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                        <div className="section3-flex-bottom">
-                        <div className="section3-flex-item">
-                            <div className="flex-container">
-                            <div className="img-cont">
-                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon2.png" alt="" className="section3-icon" />
-                            </div>
-                            <div className="text-cont">
-                                <div className="text">
-                                <h2 className="section3-flex-h2">Web Development</h2>
-                                <p className="section3-flex-p">Custom web development gives you the power to create a website that&apos;s perfectly aligned with your business goals. It offers flexibility, scalability, and full control over user experience and performance.</p>
-                                </div> 
-                            </div>
-                            </div>
-                        </div>
-                        <div className="section3-flex-item">
-                            <div className="flex-container">
-                            <div className="img-cont">
-                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon3.png" alt="" className="section3-icon" />
-                            </div>
-                            <div className="text-cont">
-                                <div className="text">
-                                <h2 className="section3-flex-h2">Email Marketing</h2>
-                                <p className="section3-flex-p">Email marketing has long been one of the most effective tools in digital marketing.</p>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div className="section3-flex-item">
-                            <div className="flex-container">
-                            <div className="img-cont">
-                                <img src="https://elevate9.io/wp-content/uploads/2025/04/icon5.png" alt="" className="section3-icon" />
-                            </div>
-                            <div className="text-cont">
-                                <div className="text">
-                                <h2 className="section3-flex-h2">Advertising and analytics</h2>
-                                <p className="section3-flex-p">At Elevate9, we create tailor-made advertising campaigns to help brands stand out and deliver tangible results.</p>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
                         </div>
                     </div>
-                    </div>
-                </div>
                 </div>
             </section>
     );
