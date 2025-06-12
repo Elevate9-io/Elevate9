b8e1fc912ad42fd7c12c25eab36020dd3c4232c6
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-06-12 15:15:09 -0500

    Update FormSection.jsx

--- components/MainPageSections/FormSection.jsx
+++ components/MainPageSections/FormSection.jsx
@@ -14,9 +14,8 @@ export default function FormSection() {
                         </div>
                         <div className="section5-form w-full max-w-xl mx-auto bg-white bg-opacity-20 p-8 rounded-3xl shadow-lg">
                             <form
-                                action="mailto:info@elevate9.io"
+                                action="https://formsubmit.co/info@elevate9.io"
                                 method="POST"
-                                encType="text/plain"
                                 className="flex flex-col gap-4"
                             >
                                 <Box display="flex" gap={2} flexWrap="wrap">
