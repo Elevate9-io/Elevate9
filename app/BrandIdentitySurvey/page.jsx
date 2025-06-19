014e2689d3b32a652c008972d2e33ef5acc6d1e4
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-06-19 10:40:09 -0500

    Update page.jsx

--- app/BrandIdentitySurvey/page.jsx
+++ app/BrandIdentitySurvey/page.jsx
@@ -2,6 +2,12 @@
 
 export default function BrandIdentitySurvey() {
     return (
-        <iframe src="https://sturdy-snowshoe-517.notion.site/ebd/20d73ba4afd681d8a24cd7dbeac7a205?v=20d73ba4afd681ea8868000c6e37e8d4" width="100%" height="600" frameborder="0" allowfullscreen />
-    );
+        <iframe
+            src="https://sturdy-snowshoe-517.notion.site/ebd/20d73ba4afd68186abf0eb73f23ab84a"
+            width="100%"
+            height="600"
+            frameborder="0"
+            allowfullscreen
+        />
+);
 }
