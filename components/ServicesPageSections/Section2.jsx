9afe73afbb5667fa95c3be9143fa2a26553f8db0
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-06-23 14:52:48 -0500

    Update Section2.jsx

--- components/ServicesPageSections/Section2.jsx
+++ components/ServicesPageSections/Section2.jsx
@@ -4,7 +4,7 @@ export default function Section2() {
   return (
     <section
       id="section2"
-      className="relative w-full min-h-screen bg-[url('https://elevate9.io/wp-content/uploads/2025/03/shutterstock_1523729564-scaled.jpg')] bg-cover bg-center"
+      className="relative w-full min-h-screen bg-[url('/images/modern_architecture_variant.jpg')] bg-cover bg-center"
     >
       <div className="absolute inset-0 bg-black/50 z-0" />
       <div className="relative z-10 px-8 md:px-[233px] py-24 text-white flex flex-col justify-center gap-24">
