2d6d46ae09c9c5936efb3fe51d1357859167b2bd
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-06-23 14:53:17 -0500

    Update Section3.jsx

--- components/ServicesPageSections/Section3.jsx
+++ components/ServicesPageSections/Section3.jsx
@@ -5,7 +5,7 @@ export default function Section3() {
   return (
     <section
       id="section3"
-      className="relative w-full min-h-screen bg-[url('https://elevate9.io/wp-content/uploads/2025/03/pexels-reynaldoyodia-31036787-1-scaled.jpg')] bg-cover bg-center"
+      className="relative w-full min-h-screen bg-[url('/images/space_shuttle_launch_realistic_bw.jpg')] bg-cover bg-center"
     >
       <div className="absolute inset-0 bg-black/50 z-0" />
       <div className="relative z-10 px-8 md:px-[233px] py-24 text-white">
